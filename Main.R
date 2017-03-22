e18ad9a67afa8c207e0bc0e3d4a74203416525e1
Herrick Wolber <wolbereric@gmail.com>
2017-03-22 17:52:09 +0100

    22

--- Main.R
+++ Main.R
@@ -4,7 +4,7 @@ library(ggplot2)
 # Mettre la racin du projet
 setwd("/Users/Rico/GitHubs_Repo/ter-ove-2017/")
 
-donne <- read.csv(file="data.csv",head=TRUE,sep="\t")
+#donne <- read.csv(file="data.csv",head=TRUE,sep="\t")
 # Generer des données de gens qui ont un boulot vs d'autre non
 # ensembleDeDonnee <- sample(0:1, size=100, replace= TRUE)
 
@@ -18,3 +18,27 @@ donne <- read.csv(file="data.csv",head=TRUE,sep="\t")
 
 # Moyenne des diplomés
 #insertionDip <- mean(donne$isDiplome)
+
+q6_5 <- sample(1:13, size=40, replace= TRUE)
+q4_3 <- sample(1:3, size=40, replace= TRUE)
+general <- data.frame(q4_3, q6_5)
+print(general)
+#general$q4_3=ifelse(general$q4_3==1, "oui", "non") 
+#head(general)
+t <- aggregate(x=general, by=list(general$q4_3), FUN =mean)
+print(t)
+
+moyenne<- function(tableau, sub){
+  mo<-(nrow(sub)*100)/nrow(tableau)
+  return(mo)
+}
+insertion<-subset(general, q4_3 == 1)
+moy<-moyenne(general, insertion)
+print(moy)
+
+#moy2 <- aggregate(x=general, by= list(general$q4_3), mean) 
+#print(moy2)
+
+
+tableauFinal <- cbind("taux d'insertion 18 mois", Universite=moy)
+print(tableauFinal)
